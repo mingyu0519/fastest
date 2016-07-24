2948b1d26960568c5931018276014f42515507ea
Mingyu Wang <wang.mingyu@rytong.com>
2016-07-24 19:45:41 +0800

    add jdbc select action

--- util/jdbc_util.py
+++ util/jdbc_util.py
@@ -1,3 +1,26 @@
 # coding=utf-8
-
+import mysql.connector
 # 添加数据库操作
+
+class mysql_conn():
+    def __init__(self,user,password,database):
+        self.user = user
+        self.password = password
+        self.database = database
+
+    def select(self,sql):
+        try:
+            conn = mysql.connector.connect(user=self.user,password=self.password,database=self.database,use_unicode=True)
+            cursor = conn.cursor()
+            cursor.execute(sql)
+            values = cursor.fetchall()
+        except Exception as e:
+            print e
+        finally:
+            cursor.close()
+            conn.close()
+        return values
+
+mysql_conn = mysql_conn(user='root',password='',database='test')
+values = mysql_conn.select(sql='select * from myclass')
+print values
