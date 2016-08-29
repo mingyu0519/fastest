590d00758bfbd1b0cc7a4eeed98ceddbd38d305b
Mingyu Wang <mingyu0519@gmail.com>
2016-08-29 22:45:35 +0800

    add update

--- util/jdbc_util.py
+++ util/jdbc_util.py
@@ -21,6 +21,18 @@ class mysql_conn():
             conn.close()
         return values
 
+    def update(self,sql):
+        try:
+            conn = mysql.connector.connect(user=self.user,password=self.password,database=self.database,use_unicode=True)
+            cursor = conn.cursor()
+            cursor.execute(sql)
+        except Exception as e:
+            print e
+        finally:
+            cursor.close()
+            conn.close()
+        return values
+
 mysql_conn = mysql_conn(user='root',password='',database='test')
 values = mysql_conn.select(sql='select * from myclass')
 print values
